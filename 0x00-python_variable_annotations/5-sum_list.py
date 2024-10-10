a101f8f7a1ed9db58fff1487a42e7d40cb391835
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-10 19:17:37 +0100

    alx tasks

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
-def sum_list(input_list: float = []) -> float:
-     """Return the sum of two float numbers."""
-     return sum(input_list)  
+"""import list"""
+from typing import List
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of a list of float numbers."""
+    return sum(input_list)  
