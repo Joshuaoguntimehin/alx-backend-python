2940fd22e0e4a423166bb2d7b47ca8be43673e4e
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-14 23:25:35 +0100

    alx tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""index net"""
 import asyncio
 import random
 import heapq
+"""index net"""
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> list:
