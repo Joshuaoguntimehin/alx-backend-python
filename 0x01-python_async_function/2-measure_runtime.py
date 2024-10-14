99562eb4314091112e6a1b26ec97dd4e5ae662fa
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-14 23:37:47 +0100

    alx tasks

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,7 +10,6 @@ def measure_time(n: int, max_delay: int) -> float:
     start =  time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
-    print(f'Task completed in {end - start:.2f} seconds.')
     
     total_time = end - start
     return total_time / n
