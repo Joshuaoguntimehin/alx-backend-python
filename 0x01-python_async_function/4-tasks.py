9e5f5596c6f2728b0bf8d8cb03954ae70da9cf4e
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-14 23:26:59 +0100

    alx tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,10 +7,12 @@ import heapq
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> list:
+    """index x"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
     
     for task in asyncio.as_completed(tasks):
+        """index h"""
         delay = await task
         heapq.heappush(delays, delay)
     
