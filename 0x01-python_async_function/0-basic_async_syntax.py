367c49d4bcf008b21c02237285a17b954ddd6655
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-15 00:55:53 +0100

    alx tasks

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 """Test file for  output of the wait_n coroutine"""
 import asyncio
 import random
-"""Test file for  output of the wait_n coroutine"""
+'''Test file for  output of the wait_n coroutine'''
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that waits for a random delay and returns it."""
-    delay = random.uniform(0, max_delay)  # Generate a random float between 0 and max_delay
+    '''Asynchronous coroutine that waits for a random delay and returns it.'''
+    delay = random.uniform(0, max_delay)  
+    '''Generate a random float between 0 and max_delay'''
     await asyncio.sleep(delay)  # Wait asynchronously for the delay
     return delay
