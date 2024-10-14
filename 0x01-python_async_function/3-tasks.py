7a0755f572719c5e9c7638cd1aeced4ceb31f9ca
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-14 23:51:38 +0100

    alx tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,11 +6,13 @@ import random
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def task_wait_random(max_delay: int) -> float:
     """Asynchronous for a random delay between 0 and max_delay."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 def create_task(max_delay: int) -> asyncio.Task:
     """Creates and for the wait_random coroutine."""
