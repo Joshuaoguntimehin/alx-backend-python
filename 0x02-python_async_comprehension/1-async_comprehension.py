9748ec9ac03f4ceb01043e8d072aa2f6d1830c64
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-16 01:09:42 +0100

    alx tasks

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ import random
 """import statement"""
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
     random_number = [num async for num in async_generator()]
     return random_number
- 
