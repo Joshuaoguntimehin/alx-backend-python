5eb8faa46e41a001848261c1f1997693ae7b4e32
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-16 01:19:12 +0100

    alx tasks

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,8 +6,9 @@ import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime():
-    """Measure total runtime of running async_comprehension 4 times in parallel."""
+    """Measure total runtime of  4 times in parallel."""
     start_time = asyncio.get_event_loop().time()  # Record start time
 
     # Run async_comprehension 4 times concurrently using asyncio.gather
